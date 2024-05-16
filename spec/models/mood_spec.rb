a2ff9b0f326765acb7df6cdd8e8ae37bf852231f
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-05-16 15:22:33 -0400

    test: model validations

--- spec/models/mood_spec.rb
+++ spec/models/mood_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Mood, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:user_id) }
+  it { should validate_presence_of(:mood) }
+  it { should validate_presence_of(:date) }
 end
